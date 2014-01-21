51176a8ab5f8f0a52ae28aba85985e8ebca2ec35
Pieter <info@pieterbos.me>
2014-01-21 13:34:35 +0100

    A bit of implementing states in the clienthandler

---
+++ src/rolit/model/networking/server/InitialClientHandlerState.java
@@ -0,0 +1,16 @@
+package rolit.model.networking.server;
+
+import rolit.model.networking.client.HandshakePacket;
+
+public class InitialClientHandlerState extends ClientHandlerState {
+    public InitialClientHandlerState(ClientHandler handler) {
+        super(handler);
+    }
+
+    @Override
+    public ClientHandlerState handshake(HandshakePacket packet) {
+
+
+        return this;
+    }
+}
