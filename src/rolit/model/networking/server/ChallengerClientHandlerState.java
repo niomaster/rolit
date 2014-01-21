b6e20c4276b8ef59e04035b80c5e7a83980d11aa
Pieter <info@pieterbos.me>
2014-01-21 14:15:15 +0100

    ???

---
+++ src/rolit/model/networking/server/ChallengerClientHandlerState.java
@@ -0,0 +1,14 @@
+package rolit.model.networking.server;
+
+import rolit.model.networking.client.StartGamePacket;
+
+public class ChallengerClientHandlerState extends ClientHandlerState {
+    public ChallengerClientHandlerState(ClientHandler handler) {
+        super(handler);
+    }
+
+    @Override
+    public ClientHandlerState startGame(StartGamePacket packet) {
+        return new GameClientHandlerState();
+    }
+}
---
+++ src/rolit/model/networking/server/GameClientHandlerState.java
@@ -0,0 +1,9 @@
+package rolit.model.networking.server;
+
+public class GameClientHandlerState extends ClientHandlerState {
+    public GameClientHandlerState(ClientHandler handler) {
+        super(handler);
+    }
+
+
+}
