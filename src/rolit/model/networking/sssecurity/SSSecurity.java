7742f1457a972629680c3c6460812e8a6e582d3e
Laurens Rouw <jlk.rouw@gmail.com>
2014-01-21 20:06:05 +0100

    intermediate

---
+++ src/rolit/model/networking/sssecurity/SSSecurity.java
@@ -0,0 +1,7 @@
+package rolit.model.networking.sssecurity;
+
+/**
+ * Created by laurens on 1/21/14.
+ */
+public class SSSecurity {
+}
---
+++ src/rolit/util/Crypto.java
@@ -0,0 +1,7 @@
+package rolit.util;
+
+/**
+ * Created by laurens on 1/21/14.
+ */
+public class Crypto {
+}
